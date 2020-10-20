d8feeb3d9f9ee224f792369c874130abbd31d648
ikechukwuhenry <ikechukwuhenry@ymail.com>
2020-10-20 13:39:06 +0100

    Initial Commit

---
+++ CheckBoxTemplate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CheckBoxTemplate
+//
+//  Created by ik on 10/20/20.
+//  Copyright © 2020 Mambilla. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
